7e350f9a29770dc2f2ebf205662cc7765da0f450
dylantypo <dylantylerposner@gmail.com>
2021-04-17 05:12:12 -0400

    updates to spacing and speed

--- js/AnimatedName.js
+++ js/AnimatedName.js
@@ -5,7 +5,7 @@ class SplitText extends Component {
     return /*#__PURE__*/(
       React.createElement("span", { "aria-label": this.props.copy, role: this.props.role },
       this.props.copy.split("").map(function (char, index) {
-        let style = { "animation-delay": 0.5 + index / 10 + "s" };
+        let style = { "animation-delay": 0.2 + index / 10 + "s" };
         return /*#__PURE__*/React.createElement("span", {
           "aria-hidden": "true",
           key: index,
