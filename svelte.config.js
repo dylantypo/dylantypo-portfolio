8c77ed9a96b73fc8af5786802b040f248f82024f
dylantypo <dylantylerposner@gmail.com>
2023-05-30 10:37:15 -0400

    updated commit

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,17 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter()
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            fallback: '200.html',
+            pages: 'build',
+            assets: 'build',
+            // by default, `static` will output client and server
+            // but the server runtime is unnecessary when prerendering
+            prerender: true
+        }),
+    },
 };
 
 export default config;
