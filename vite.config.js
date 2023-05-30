302a921df5c09838d0a1311636d9ad5c5d23ee02
dylantypo <dylantylerposner@gmail.com>
2023-05-30 12:34:38 -0400

    working site commit

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,7 @@ const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: (id) => {
-                    if (id.includes('node_modules')) {
-                        // put all vendor code into a separate chunk
-                        return 'vendor';
-                    }
-                }
-            }
-        }
-    }
+	}
 };
 
-export default config
+export default config;
