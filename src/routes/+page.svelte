d772194cad1e382151a5cdeb99fd4605b44e214a
dylantypo <dylantylerposner@gmail.com>
2023-06-07 16:40:52 -0400

    uno reverse

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
+    // import SnakeGame from '$lib/snakeGame.svelte';
+    import RetroConsole from '$lib/RetroConsole.svelte';
+	import ToolBar from '$lib/ToolBar.svelte';
+    import MainContent from '$lib/mainContent.svelte';
     import { onMount } from 'svelte';
-    // let SnakeGame;
-    let RetroConsole;
-	let ToolBar;
-    let MainContent;
 
-    onMount(async () => {
-        // const sgModule = await import('$lib/snakeGame.svelte');
-        // SnakeGame = sgModule.default;
-        const rcModule = await import('$lib/RetroConsole.svelte');
-        RetroConsole = rcModule.default;
-        const tbModule = await import('$lib/ToolBar.svelte');
-        ToolBar = tbModule.default;
-        const mcModule = await import('$lib/mainContent.svelte');
-        MainContent = mcModule.default;
+    onMount(() => {
         window.scrollTo(0, 0);
     });
 </script>
